552f5943a5886667c514a1c1513dd8e576442b01
c.zeng <c.zeng@4flow.com>
2023-11-30 18:07:48 +0100

    change user pass

--- backend/coffeerDateRegister.js
+++ backend/coffeerDateRegister.js
@@ -7,7 +7,7 @@ async function main() {
  * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
  * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
  */
-const uri = "mongodb+srv://chaowow:VHhRjzyNfFHWW51K@cluster0.ups2ujp.mongodb.net/test?retryWrites=true&w=majority";
+//const uri = "mongodb+srv://<user>:<pass>@cluster0.ups2ujp.mongodb.net/test?retryWrites=true&w=majority";
   
   const client = new MongoClient(uri);
  
