8427c0b321e43fe2cf70b597f4d21bd00835ac72
c.zeng <c.zeng@4flow.com>
2023-12-01 16:14:41 +0100

    user dotenv to pass the db authentication

--- backend/coffeerDateRegister.js
+++ backend/coffeerDateRegister.js
@@ -1,5 +1,7 @@
 // connection to db 
 // insert the data to the table 
+require('dotenv').config();
+
 const {MongoClient} = require('mongodb');
 async function main() {
   
@@ -7,8 +9,14 @@ async function main() {
  * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
  * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
  */
-//const uri = "mongodb+srv://<user>:<pass>@cluster0.ups2ujp.mongodb.net/test?retryWrites=true&w=majority";
-  
+
+
+const dbuser = process.env.dbusername;
+const password = process.env.dbpassword;
+
+console.log('dbuser:' + dbuser + " pass: " +password)
+
+const uri = `mongodb+srv://${dbuser}:${password}@cluster0.ups2ujp.mongodb.net/`;//todo need env to parse
   const client = new MongoClient(uri);
  
   try {
