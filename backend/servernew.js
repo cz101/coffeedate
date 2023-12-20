f88274eb5f3c18f76323f100ccab3ed4d0fe8d22
c.zeng <c.zeng@4flow.com>
2023-12-20 22:30:35 +0100

    display the data on the page, fix  to load css file

--- backend/servernew.js
+++ backend/servernew.js
@@ -8,20 +8,20 @@ const server = http.createServer((req ,res)=>{
   // res.writeHead(200,{'Content-Type': 'application/json'})
   // res.end(JSON.stringify(user));
 
-  //to do routing 
+  //to do  dynamic routing 
 
   if (req.url && req.method.toLowerCase() ==='get'){
    if (req.url ==="/user/adminuser")
     {
         getAllUsers(req,res)
     }
-    else if (req.url.match(/\/user\/employee\/([0-9]+)/)){
+   else if (req.url.match(/\/user\/employee\/([0-9]+)/)){
       {
          const id = req.url.split('/')[3]
          getUserById(req,res,id)
       }
     }
-    else if ( req.url ==="/user/userregister"&& req.method.toLowerCase() ==='get'){
+   else if ( req.url ==="/user/userregister"){
 
       res.writeHead( 200, {'Content-Type':'text/html'})
       fs.readFile('user/userregister.html', (error, html)=>{
@@ -35,34 +35,30 @@ const server = http.createServer((req ,res)=>{
         }
         res.end()
       })}
-      else if (req.url ==="/backend/data/"&& req.method.toLowerCase() ==='get')
+   else if (req.url ==="/backend/data/")
       {
            res.writeHead(200,{'Content-Type': 'application/json'})
-            res.end(JSON.stringify(user));
+           res.end(JSON.stringify(user));
       }
+   else if (req.url === '/user/css/adminuser.css'||req.url === '/user/css/userprofile.css') {
+      res.writeHead(200, {'Content-Type': 'text/css'}); 
+      fs.readFile('user/css/adminuser.css', (error, data)=>{
+        if(error){
+          res.writeHead(404)
+          res.write('Error : File Not Found')
+        }
+        else{          
+          res.write(data)
+        }        
+        res.end();
+      })
+    }
   } //end of get 
   else if (req.url ==="user/userregister"&& req.method.toLowerCase() ==='post'){
-
      //let form =  new formidable.IncomingForm()
      //createrUser(req,res,form)
      createNewUser(req,res)
    }
-//   else if (req.url === '/user/css/adminuser.css'&& req.method.toLowerCase() ==='get') {
-//     fs.readFile('/user/css/adminuser.css', 
-//     function(error, page) { 
-//       if(error){
-//         res.writeHead(404)
-//         res.write('Error : File Not Found'+ error)
-//       }
-//       else{
-//         res.writeHead(200, {'Content-Type': 'text/css'}); 
-//         res.write(page); 
-//         //res.end()
-//    }
-//    res.end()
-//   }); 
-//  } 
-
 })
 
 const port = process.env.port || 5001
